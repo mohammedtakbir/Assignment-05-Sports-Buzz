976899e6b37faf6cdf605082701eb15936337c1b
mohammedtakbir <takbirhossain678@gmail.com>
2022-08-23 12:43:54 +0600

    all validation & comment added

--- js/app.js
+++ js/app.js
@@ -1,27 +1,23 @@
-let playerArray = []
-
+let playerArray = [];
 function displayPlayer(players){
     const displaySelectedPlayer = document.getElementById('selected-player');
     displaySelectedPlayer.innerText = '';
     let count = 0;
     for(const player of players){
         count++;
-        // ! validation 1
+        // * Validation
         if(count > 5){
-            // alert('you have already selected five players')
             return;
         }
         const name = player.playerName;
-
         const ul = document.createElement('ul');
         ul.innerHTML = `
-        <li class="mb-6">
+        <li class="mb-4">
             <span class="text-gray-400 mr-5 font-bold text-2xl">${count}.</span>
             <span class="text-white font-normal text-xl">${name}</span>
         </li>
         `;
         displaySelectedPlayer.appendChild(ul);
-        // console.log(ul.childNodes)
     }
 }
 
@@ -30,59 +26,53 @@ function addToSelect(clickBtn){
     const playerObject = {
         playerName: playerName
     }
+    // * Validation
     if(playerArray.length < 5){
         playerArray.push(playerObject);
-        // ! validation
+        // * For disable selected button
         if(clickBtn.disabled = true){
             clickBtn.parentElement.parentElement.children[1].children[2].style.backgroundColor = 'gray';
         }
     }
     else{
-        alert("You've Already Choose Five Players");
+        alert('You Already Select Five Players');
+        return;
     }
     displayPlayer(playerArray);
 }
 
-// ? calculation per player
+// * Calculate player Expenses
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const selectedPlayer = playerArray.length;
     const perPlayerBudget = getInputById('player-budget');
-    // ! validation
-    if(isNaN(perPlayerBudget)){
-        alert('Please Enter Number');
-        return;
-    }
-    else if(perPlayerBudget < 0){
+    // * validation
+    if((isNaN(perPlayerBudget)) || (perPlayerBudget < 0)){
         alert('Please Enter Valid Number');
         return;
     }
+    else if(selectedPlayer === 0){
+        alert('Please Select Player');
+        return;
+    }
     const totalPlayerExpense = selectedPlayer * perPlayerBudget;
-
-    const previousExpense = document.getElementById('player-expense');
-    previousExpense.innerText = totalPlayerExpense;
+    setTextElementById('player-expense', totalPlayerExpense);
 })
 
-// ? total calculation
+// * total calculation
 document.getElementById('total-calculate-btn').addEventListener('click', function(){
     const playerExpenses = getTextElementById('player-expense');
     const managerExpenses = getInputById('manager-expense');
     const coachExpenses = getInputById('coach-expense');
-    // ! validation
-    if((isNaN(managerExpenses)) || (isNaN(coachExpenses))){
-        alert('Please Enter Number');
-        return;
-    }
-    else if((managerExpenses < 0) || (coachExpenses < 0)){
+    // * validation
+    if(((isNaN(managerExpenses)) || (managerExpenses < 0)) || ((isNaN(coachExpenses)) || (coachExpenses < 0))){
         alert('Please Enter Valid Number');
         return;
     }
-    else if(playerExpenses < 0){
-        alert('You Have to Calculate Player Expenses First.');
-        return
+    else if(playerExpenses === 0){
+        alert('You Have to Calculate Player Expenses First');
+        return;
     }
     const finalTotal = playerExpenses + managerExpenses + coachExpenses;
-    const totalExpense = document.getElementById('total-expense');
-
-    totalExpense.innerText = finalTotal;
+    setTextElementById('total-expense', finalTotal)
 })
 
